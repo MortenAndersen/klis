8925576b6f590578ca3ad08c59fdc7d65d29c6a1
Morten Andersen <morten@hjemmesider.dk>
2024-06-27 09:21:14 +0200

    Update bobler-loop.php
    'order'

--- template-parts/content/bobler-loop.php
+++ template-parts/content/bobler-loop.php
@@ -30,7 +30,9 @@ elseif (is_page( 106 )) {
 $args = array(
     'post_type' => 'bobler',
     'post__not_in' => array($data),
-    'posts_per_page' => -1
+    'posts_per_page' => -1,
+    'orderby' => 'menu_order',
+    'order' => 'ASC', 
 );
 $the_query = new WP_Query( $args ); 
 
