d0e73b154fb698bd653d9741d3f5a69da6b41393
Morten Andersen <morten@hjemmesider.dk>
2024-07-03 12:51:48 +0200

    ref link
    footer link

--- footer.php
+++ footer.php
@@ -7,6 +7,7 @@ if (is_active_sidebar('footer')) {
     echo '</div>';
   echo '</footer>';
 }
+echo '<div class="ref content l-wrap"><a href="https://www.hjemmesider.dk" rel="nofollow" target="_blank">Hjemmesider.dK</a></div>';
 wp_footer();
 do_action('clean_before_body_end');
 ?>
