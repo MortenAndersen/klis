d8e3520d41796a03f2e7e433ddae7345ee83f5bc
Morten Andersen <morten@hjemmesider.dk>
2025-06-21 11:29:52 +0200

    Update lovkrav-loop.php
    type_all

--- template-parts/content/lovkrav-loop.php
+++ template-parts/content/lovkrav-loop.php
@@ -21,64 +21,9 @@ if ($tjek1 || $tjek2 || $tjek3){
 
 // Filer
 echo '<div class="down-con">';
-echo '<h3>Vedtægter</h3>';
-echo do_shortcode( '[type type=vedtaegter]' );
+echo do_shortcode( '[type_all]' );
 echo '</div>';
 
-echo '<div class="down-con">';
-echo '<h3>Evalueringer</h3>';
-echo do_shortcode( '[type type=evaluering]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Ferieplan</h3>';
-echo do_shortcode( '[type type=ferie]' );
-echo '</div>';
-
-// echo '<div class="down-con">';
-// echo '<h3>Mobning</h3>';
-// echo do_shortcode( '[type type=mobning]' );
-// echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Privatlivspolitik - GDPR</h3>';
-echo do_shortcode( '[type type=GDPR]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Takster</h3>';
-echo do_shortcode( '[type type=takster]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Tilsynserklæring</h3>';
-echo do_shortcode( '[type type=tilsyn]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Politik</h3>';
-echo do_shortcode( '[type type=politik]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Udviklingsplan</h3>';
-echo do_shortcode( '[type type=udvikling]' );
-echo '</div>';
-
-echo '<div class="down-con">';
-echo '<h3>Undervisning</h3>';
-echo do_shortcode( '[type type=undervisning]' );
-echo '</div>';
-
-echo '<div class="down-con" style="border:none;">';
-echo '<h3>Undervisningsmiljøvurdering</h3>';
-echo do_shortcode( '[type type=umv]' );
-echo '</div>';
-
-echo '<div class="down-con" style="border:none;">';
-echo '<h3>Information</h3>';
-echo do_shortcode( '[type type=info]' );
-echo '</div>';
 
 
         echo '</article>';
